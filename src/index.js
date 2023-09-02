ccfe861e19ccf7fbad04568315b80ac5d3911a35
Manaci4466 <Manasseh.Ophalfens@outlook.com>
2023-09-02 02:07:58 +0200

    pls let me sleep everything send

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const observer = (classname) => {
+    return { observer: new IntersectionObserver((entries) => {
+                entries.forEach((entry) => {
+                    if(entry.isIntersecting)
+                    entry.target.classList.add(classname);
+                    else{
+                        entry.target.classList.remove(classname);
+                    }
+                })
+            })
+    }
+}
+
+
+const animHandler = (nodes,classname) => {
+    
+    return {nodes,...observer(classname),
+        observe(){
+            nodes.forEach((el) => {this.observer.observe(el)})
+        }
+    };
+}
+const headerHandler = animHandler(document.querySelectorAll('.hidden'),'anim');
+headerHandler.observe();
+const nodesRight = animHandler(document.querySelectorAll('.shiftleft'),'shift');
+nodesRight.observe();
+const nodesLeft = animHandler(document.querySelectorAll('.shiftright'),'shift');
+nodesLeft.observe();
+
+lo
